a30e20c9a806bd24179517f876c749eedcfd1cfc
Shunsuke Mie <sux2mfgj@gmail.com>
2023-07-05 23:51:03 +0900

    pci: qemu-epc: wip: reserve pci resources on probe

--- drivers/pci/controller/pcie-qemu-ep.c
+++ drivers/pci/controller/pcie-qemu-ep.c
@@ -9,10 +9,20 @@
 #include <linux/pci.h>
 #include <linux/pci-epc.h>
 
+struct qemu_ep {
+	void __iomem *cfg_base;
+};
+
+#define QEMU_EP_DRV_NAME "QEMU PCIe EP driver"
+
 static int qemu_ep_write_header(struct pci_epc *epc, u8 fn, u8 vfn,
 				struct pci_epf_header *hdr)
 {
-	return -ENOTSUPP;
+	struct qemu_ep *qep = epc_get_drvdata(epc);
+
+	memcpy_toio(qep->cfg_base, hdr, sizeof(*hdr));
+
+	return 0;
 }
 
 static int qemu_ep_set_bar(struct pci_epc *epc, u8 fn, u8 vfn,
@@ -78,19 +88,72 @@ static int qemu_ep_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
 	struct device *dev = &pdev->dev;
 	struct pci_epc *epc;
+	struct qemu_ep *qep;
+	int err;
 
 	dev_info(dev, "probe is called\n");
 
+	qep = devm_kzalloc(dev, sizeof(*qep), GFP_KERNEL);
+	if (!qep) {
+		dev_err(dev, "Failed to allocate memory\n");
+		return -ENOMEM;
+	}
+
 	epc = devm_pci_epc_create(dev, &qemu_epc_ops);
 	if (IS_ERR(epc)) {
-		dev_err(dev, "failed to create epc device\n");
-		return PTR_ERR(epc);
+		dev_err(dev, "Failed to create epc device\n");
+		err = PTR_ERR(epc);
+		goto err_qep_kfree;
 	}
+
+	epc_set_drvdata(epc, qep);
 
 	// TODO above valus should be got from qemu-ep.
 	epc->max_functions = 1;
 
+	err = pci_enable_device(pdev);
+	if (err) {
+		dev_err(dev, "Cannot enable PCI device\n");
+		goto err_release_epc;
+	}
+
+	if (!(pci_resource_flags(pdev, 0) & IORESOURCE_MEM)) {
+		dev_err(dev, "Cannot find proper PCI BAR\n");
+		err = -ENODEV;
+		goto err_disable_pdev;
+	}
+
+	err = pci_request_regions(pdev, QEMU_EP_DRV_NAME);
+	if (err) {
+		dev_err(dev, "Cannot obtain PCI resources\n");
+		goto err_disable_pdev;
+	}
+
+	err = dma_set_mask(dev, DMA_BIT_MASK(64));
+	if (err) {
+		dev_err(dev, "No usable DMA configuration\n");
+		goto err_disable_pdev;
+	}
+
+	qep->cfg_base = pci_iomap(pdev, 0, PCI_CFG_SPACE_SIZE);
+	if (!qep->cfg_base) {
+		dev_err(dev, "Cannot map device registers\n");
+		err = -ENOMEM;
+		goto err_disable_pdev;
+	}
+
+	pci_set_master(pdev);
+
 	return 0;
+
+err_disable_pdev:
+	pci_disable_device(pdev);
+err_release_epc:
+	devm_pci_epc_destroy(dev, epc);
+err_qep_kfree:
+	devm_kfree(dev, qep);
+
+	return err;
 }
 
 #define PCI_DEVICE_ID_REDHAT_PCI_EP 0x0013
@@ -105,7 +168,7 @@ static const struct pci_device_id qemu_ep_id_table[] = {
 // TODO: MODULE_DEVICE_TABLE(pci, qemu_ep_id_table)
 
 static struct pci_driver qemu_ep_driver = {
-	.name = "QEMU PCIe EP driver",
+	.name = QEMU_EP_DRV_NAME,
 	.id_table = qemu_ep_id_table,
 	.probe = qemu_ep_probe,
 	//     .remove = qemu_ep_remove,
